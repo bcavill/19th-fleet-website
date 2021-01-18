892c3d0256601e0e2bb32f74bac60eb72ecef92d
ben12623 <ben.cavill@hotmail.co.uk>
2021-01-18 13:17:32 +0000

    First iteration of the 19th fleet website.

---
+++ Public/index.php
@@ -0,0 +1,62 @@
+<html>
+<head>
+    <title>19th Fleet | Arma 3 unit</title>
+    <meta name='description' content='The 19th Fleet is a serious, Story-driven, Arma 3 Milsim unit set in the events of the Halo universe during the ending years of Operation: TREBUCHET.'>
+    <meta name='keywords' content='Arma, Arma 3, 19th fleet, optre'>
+    <link rel="shortcut icon" type="image/x-icon" href="Images/19th.png" />
+    <link rel='stylesheet' href='CSS/main.css'/>
+</head>
+<body>
+    <div id='nav-bar'>
+        <a href='/'>
+            <img id='logo' src='Images/19th.png' alt='19th fleet logo'>
+            </img>
+        </a>
+        <a href='https://discord.gg/X4UqbYN'>
+            <img id='discord-logo' src='Images/discord.jpg' alt='Discord logo'></a>
+        </a>
+    </div>
+    <div id='content'>
+        <h1>The 19th Fleet is a serious, Story-driven, Milsim set in the events of the Halo universe during the ending years of Operation: TREBUCHET.</h1>
+
+        <h2>What are your requirements for joining?</h2>
+        <p>You must be willing to use Teamspeak 3 with Task Force Beta, are over the age of 16, and are at least able to attend most operations (though we do understand people are busy and allow people to submit LOAs before missions).</p>
+
+        <h2>How many people come each week?</h2>
+        <p>We have an average attendance of 50+ people per operation.</p>
+
+        <h2>When do you have operations?</h2>
+        <p>Most of our members are based out of North America, and as such our operations and training stay in the area of 5-10pm EST with our main operations starting at 7pm EST every Saturday.</p>
+
+        <h2>How long do operations typically last?</h2>
+        <p>We shoot for 2+ hours as it's enough time to brief, deploy, execute, and exfil from a multi-objective mission without making the mission seem drawn out.</p>
+
+        <h2>Do you have automatic, or user-made missions?</h2>
+        <p>We have both! Our operations are always hand-made by Zeus and follow a storyline put in place by our Creative Team that transpires over several weeks which we call "Campaigns". However, when there are no training or operations, we do host an OPTRE-based Liberation on our main server for people to enjoy.</p>
+
+        <h2>Why join the 19th Fleet?</h2>
+        <ul>
+            <li>Command staff and senior enlisted that listen to all opinions, regardless of rank or experience</li>
+            <li>Customizable armor for personalization</li>
+            <li>A broad selection of MOS' for all kinds of playstyles (30+ MOS' combined between Aviation and Marines)</li>
+            <li>Easy load mod pack that ensures no complications or issues come operation time</li>
+        </ul>
+
+        <h2>We are composed of:</h2>
+        <ul>
+            <li>UNSC Marine Corps Infantry/Force Recon</li>
+            <li>UNSC Marine Armor</li>
+            <li>UNSC Naval Air Corps</li>
+            <li>UNSC Orbital Drop Shock Troopers (ODSTs)</li>
+        </ul>
+
+        <h2>Schedule:</h2>
+        <ul>
+            <li>Weekdays: Schedule based ops system + training</li>
+            <li>Saturday: Operation (7PM EST)</li>
+            <li>Sunday: Free</li>
+
+        <p>Any additional questions can be messaged to any of the command team on our discord.</p>
+    </div>
+</body>
+</html>

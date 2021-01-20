5c264ed8cf7c9daaea5a371bae1dec6688ee793c
ben12623 <ben.cavill@hotmail.co.uk>
2021-01-20 14:17:37 +0000

    Redesigned content to be more readable.

--- Public/index.php
+++ Public/index.php
@@ -4,7 +4,7 @@
     <meta name='description' content='The 19th Fleet is a serious, Story-driven, Arma 3 Milsim unit set in the events of the Halo universe during the ending years of Operation: TREBUCHET.'>
     <meta name='keywords' content='Arma, Arma 3, 19th fleet, optre'>
     <link rel="shortcut icon" type="image/x-icon" href="Images/19th.png" />
-    <link rel='stylesheet' href='CSS/main.css'/>
+    <link rel='stylesheet' href='CSS/main.css?1'/>
 </head>
 <body>
     <div id='nav-bar'>
@@ -17,46 +17,69 @@
         </a>
     </div>
     <div id='content'>
-        <h1>The 19th Fleet is a serious, Story-driven, Milsim set in the events of the Halo universe during the ending years of Operation: TREBUCHET.</h1>
+        <div class='content-section'>
+            <h1>The 19th Fleet is a serious, Story-driven, Milsim set in the events of the Halo universe during the ending years of Operation: TREBUCHET.</h1>
+        </div>
 
-        <h2>What are your requirements for joining?</h2>
-        <p>You must be willing to use Teamspeak 3 with Task Force Beta, are over the age of 16, and are at least able to attend most operations (though we do understand people are busy and allow people to submit LOAs before missions).</p>
+        <div class='content-section'>
+            <h2>What are your requirements for joining?</h2>
+            <p>You must be willing to use Teamspeak 3 with Task Force Beta, are over the age of 16, and are at least able to attend most operations (though we do understand people are busy and allow people to submit LOAs before missions).</p>
+        </div>
 
-        <h2>How many people come each week?</h2>
-        <p>We have an average attendance of 50+ people per operation.</p>
+        <div class='content-section'>
+            <h2>How many people come each week?</h2>
+            <p>We have an average attendance of 50+ people per operation.</p>
+        </div>
 
-        <h2>When do you have operations?</h2>
-        <p>Most of our members are based out of North America, and as such our operations and training stay in the area of 5-10pm EST with our main operations starting at 7pm EST every Saturday.</p>
+        <div class='content-section'>
+            <h2>When do you have operations?</h2>
+            <p>Most of our members are based out of North America, and as such our operations and training stay in the area of 5-10pm EST with our main operations starting at 7pm EST every Saturday.</p>
+        </div>
 
-        <h2>How long do operations typically last?</h2>
-        <p>We shoot for 2+ hours as it's enough time to brief, deploy, execute, and exfil from a multi-objective mission without making the mission seem drawn out.</p>
+        <div class='content-section'>
+            <h2>How long do operations typically last?</h2>
+            <p>We shoot for 2+ hours as it's enough time to brief, deploy, execute, and exfil from a multi-objective mission without making the mission seem drawn out.</p>
+        </div>
 
-        <h2>Do you have automatic, or user-made missions?</h2>
-        <p>We have both! Our operations are always hand-made by Zeus and follow a storyline put in place by our Creative Team that transpires over several weeks which we call "Campaigns". However, when there are no training or operations, we do host an OPTRE-based Liberation on our main server for people to enjoy.</p>
+        <div class='content-section'>
+            <h2>Do you have automatic, or user-made missions?</h2>
+            <p>We have both! Our operations are always hand-made by Zeus and follow a storyline put in place by our Creative Team 
+            that transpires over several weeks which we call "Campaigns". However, when there are no training or operations, we do host an OPTRE-based Liberation on our main server for people to enjoy.</p>
+        </div>
 
-        <h2>Why join the 19th Fleet?</h2>
-        <ul>
-            <li>Command staff and senior enlisted that listen to all opinions, regardless of rank or experience</li>
-            <li>Customizable armor for personalization</li>
-            <li>A broad selection of MOS' for all kinds of playstyles (30+ MOS' combined between Aviation and Marines)</li>
-            <li>Easy load mod pack that ensures no complications or issues come operation time</li>
-        </ul>
+        <div class='content-section'>
+            <h2>Why join the 19th Fleet?</h2>
+            <ul>
+                <li>Command staff and senior enlisted that listen to all opinions, regardless of rank or experience</li>
+                <li>Customizable armor for personalization</li>
+                <li>A broad selection of MOS' for all kinds of playstyles (30+ MOS' combined between Aviation and Marines)</li>
+                <li>Easy load mod pack that ensures no complications or issues come operation time</li>
+            </ul>
+        </div>
 
-        <h2>We are composed of:</h2>
-        <ul>
-            <li>UNSC Marine Corps Infantry/Force Recon</li>
-            <li>UNSC Marine Armor</li>
-            <li>UNSC Naval Air Corps</li>
-            <li>UNSC Orbital Drop Shock Troopers (ODSTs)</li>
-        </ul>
+        <div class='content-section'>
+            <h2>We are composed of:</h2>
+            <ul>
+                <li>UNSC Marine Corps Infantry/Force Recon</li>
+                <li>UNSC Marine Armor</li>
+                <li>UNSC Naval Air Corps</li>
+                <li>UNSC Orbital Drop Shock Troopers (ODSTs)</li>
+            </ul>
+        </div>
 
-        <h2>Schedule:</h2>
-        <ul>
-            <li>Weekdays: Schedule based ops system + training</li>
-            <li>Saturday: Operation (7PM EST)</li>
-            <li>Sunday: Free</li>
+        <div class='content-section'>
+            <h2>Schedule:</h2>
+            <ul>
+                <li>Weekdays: Schedule based ops system + training</li>
+                <li>Saturday: Operation (7PM EST)</li>
+                <li>Sunday: Free</li>
+            </ul>
+        </div>
 
-        <p>Any additional questions can be messaged to any of the command team on our discord.</p>
+        <div class='content-section'>
+            <p>Any additional questions can be messaged to any of the command team on our discord.</p>
+        </div>
+        <br>
     </div>
 </body>
 </html>
